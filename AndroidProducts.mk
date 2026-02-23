e77e3c32b13b4633cc3755dfdc19de568810b663
elmendezz <165435968+elmendezz@users.noreply.github.com>
2026-02-22 19:32:25 -0800

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,20 +1,6 @@
 # Version: 1
 # Changelog:
-# - Se cambiaron las referencias de james a pettyl en PRODUCT_MAKEFILES y COMMON_LUNCH_CHOICES.
-
-# [cite_start]Copyright (C) 2019 The LineageOS Project [cite: 4]
-#
-# [cite_start]Licensed under the Apache License, Version 2.0 (the "License"); [cite: 4]
-# [cite_start]you may not use this file except in compliance with the License. [cite: 5]
-# [cite_start]You may obtain a copy of the License at [cite: 6]
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# [cite_start]See the License for the specific language governing permissions and [cite: 7]
-# [cite_start]limitations under the License. [cite: 7]
+# - Definición inicial de PRODUCT_MAKEFILES para pettyl.
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_pettyl.mk
@@ -22,4 +8,4 @@ PRODUCT_MAKEFILES := \
 COMMON_LUNCH_CHOICES := \
     lineage_pettyl-user \
     lineage_pettyl-userdebug \
-    [cite_start]lineage_pettyl-eng [cite: 4]
+    lineage_pettyl-eng
