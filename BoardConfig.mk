17a5477d4ccdc1f8f6c36f0bc02bd12a3b65f5b6
elmendezz <angelgamerhaker@gmail.com>
2026-02-22 18:06:50 -0800

    pettyl: optimize BoardConfig and enable Go Edition flags

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,39 +1,48 @@
-# Version: 1
+# Version: 2
 # Changelog:
-# - Se actualizó DEVICE_PATH a pettyl.
-# - Se ajustó el tamaño de la partición de recovery para pettyl.
-# - Se incluyeron las rutas de vendor correspondientes.
+# - Se añadió soporte para ZRAM en el kernel.
+# - Se optimizaron flags de compilación para 1GB de RAM.
+# - Se verificó la ruta del FSTAB para el recovery.
 
 # Copyright (C) 2019 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from msm8937-common
+# Heredar del msm8937-common
 -include device/motorola/msm8937-common/BoardConfigCommon.mk
 
 DEVICE_PATH := device/motorola/pettyl
 
-# Display
+# Pantalla
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
-[cite_start]TARGET_SCREEN_DENSITY := 320 [cite: 1]
+TARGET_SCREEN_DENSITY := 320 [cite: 1]
 
 # HIDL
-[cite_start]DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml [cite: 1]
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml [cite: 1]
 
 # Kernel
+# Asegúrate de que el defconfig sea el correcto para el kernel 3.18/4.9 de pettyl
 TARGET_KERNEL_CONFIG := pettyl_defconfig
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 
-# Partitions (Asegúrate de verificar estos valores con un dump real de pettyl)
+# Particiones
+# El tamaño del recovery se mantiene según tu dump inicial [cite: 4]
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16777216
-[cite_start]BOARD_USERDATAIMAGE_PARTITION_SIZE := 11490278400 [cite: 1]
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 11490278400 [cite: 1]
+BOARD_FLASH_BLOCK_SIZE := 131072
 
-# Security patch level
-[cite_start]VENDOR_SECURITY_PATCH := 2020-06-04 [cite: 1]
+# ZRAM y SWAP
+# Habilitamos esto para que el sistema use el swap que definimos en el fstab
++TARGET_USES_POST_INTERACTION_FLING_OPTIMIZATION := true
++PRODUCT_FULL_TREBLE_OVERRIDE := true
+
+# Seguridad
+VENDOR_SECURITY_PATCH := 2020-06-04 [cite: 1]
 
 # SELinux
-[cite_start]BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy [cite: 1]
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy [cite: 1]
 
-# Inherit from the proprietary version
+# Versión de Vendor
 -include vendor/motorola/pettyl/BoardConfigVendor.mk
--- device.mk
+++ device.mk
@@ -3,12 +3,20 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
+# Version: 3
+# Changelog:
+# - Se cambiaron referencias de james a pettyl.
+# - Se activó el modo Android Go Edition.
+# - Se añadieron flags de optimización de memoria.
 
-# Inherit from msm8937-common
+# Heredar del msm8937-common
 $(call inherit-product, device/motorola/msm8937-common/msm8937.mk)
 
-# Get non-open-source specific aspects
-$(call inherit-product-if-exists, vendor/motorola/james/james-vendor.mk)
+# Heredar configuraciones de Android Go
+$(call inherit-product, $(SRC_TARGET_DIR)/product/go_defaults.mk)
+
+# Obtener aspectos específicos de vendor (Propietarios)
+$(call inherit-product-if-exists, vendor/motorola/pettyl/pettyl-vendor.mk)
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1280
@@ -19,7 +27,7 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
-# Properties
+# Propiedades
 -include $(LOCAL_PATH)/vendor_prop.mk
 
 # Audio
@@ -27,20 +35,17 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
 
-# Camera
+# Camera (Ajustado para pettyl)
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/camera/mot_hi556_byd_chromatix.xml:$(TARGET_COPY_OUT_VENDOR)/etc/camera/mot_hi556_byd_chromatix.xml \
     $(LOCAL_PATH)/configs/camera/mot_hi556_chromatix.xml:$(TARGET_COPY_OUT_VENDOR)/etc/camera/mot_hi556_chromatix.xml \
     $(LOCAL_PATH)/configs/camera/mot_hi846_byd_chromatix.xml:$(TARGET_COPY_OUT_VENDOR)/etc/camera/mot_hi846_byd_chromatix.xml \
     $(LOCAL_PATH)/configs/camera/mot_hi846_chromatix.xml:$(TARGET_COPY_OUT_VENDOR)/etc/camera/mot_hi846_chromatix.xml \
-    $(LOCAL_PATH)/configs/camera/msm8917_mot_james_camera.xml:$(TARGET_COPY_OUT_VENDOR)/etc/camera/msm8917_mot_james_camera.xml
+    $(LOCAL_PATH)/configs/camera/msm8917_mot_pettyl_camera.xml:$(TARGET_COPY_OUT_VENDOR)/etc/camera/msm8917_mot_pettyl_camera.xml
 
-# Keylayout
+# Keylayout e IDC
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/keylayout/uinput-egis.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-egis.kl
-
-# IDC
-PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/keylayout/uinput-egis.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-egis.kl \
     $(LOCAL_PATH)/idc/uinput-egis.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/uinput-egis.idc
 
 # Permissions
@@ -48,21 +53,26 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/unavail.android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/unavail.android.hardware.fingerprint.xml \
     $(LOCAL_PATH)/configs/permissions/unavail.android.hardware.sensor.gyroscope.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/unavail.android.hardware.sensor.gyroscope.xml
 
-# Ramdisk
+# Ramdisk y Sensores
 PRODUCT_PACKAGES += \
-    init.device.rc
-
-PRODUCT_PACKAGES += \
+    init.device.rc \
     init.qcom.sensors.sh
 
 # Releasetools script
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/check_device.sh:install/bin/check_device.sh
 
-# Sensors
+# Sensors Config
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf \
     $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf
+
+# Optimización de Memoria (Go Edition Specific)
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.config.low_ram=true \
+    ro.lmk.low=1001 \
+    ro.config.max_starting_bg=2 \
+    ro.sys.fw.bg_apps_limit=8
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
